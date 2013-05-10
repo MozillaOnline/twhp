7c2b60bed350ec7474815c57f6f449326cbb6546
Mozilla Online <info-cn@mozilla.com>
2013-05-10 16:51:12 +0800

    Bug 770: sync with distribution.ini

--- defaults/preferences/pref.js
+++ defaults/preferences/pref.js
@@ -2,3 +2,12 @@ pref("extensions.twhomepage.autoSetHomepage",false);
 pref("extensions.twhomepage.homepage", "chrome://twhomepage/locale/twhomepage.properties");
 pref("extensions.twhomepage.abouturl", "chrome://twhomepage/locale/twhomepage.properties");
 pref("browser.startup.homepage", "chrome://twhomepage/locale/twhomepage.properties");
+
+pref("browser.search.defaultenginename", "data:text/plain,browser.search.defaultenginename=Google");
+pref("browser.search.order.1", "data:text/plain,browser.search.order.1=Google");
+pref("browser.search.order.2", "data:text/plain,browser.search.order.2=Yahoo");
+pref("browser.search.order.3", "data:text/plain,browser.search.order.3=Wikipedia (zh)");
+pref("browser.search.order.4", "data:text/plain,browser.search.order.4=Creative Commons");
+pref("browser.search.order.5", "data:text/plain,browser.search.order.5=Findbook");
+pref("browser.search.order.6", "data:text/plain,browser.search.order.6=Yahoo!奇摩拍賣");
+pref("browser.search.order.7", "data:text/plain,browser.search.order.7=Yahoo!奇摩知識+");
